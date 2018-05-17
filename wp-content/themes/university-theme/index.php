cdf1ed14f2b082a5a6e185ce785952d4498ac383
sevketyilmaz <sevketyilmaz@gmail.com>
2018-05-17 10:08:28 +0000

    03-009-PHP arrays.

--- wp-content/themes/university-theme/index.php
+++ wp-content/themes/university-theme/index.php
@@ -1,2 +1,16 @@
 <h1><?php bloginfo('name'); ?></h1>
 <p><?php bloginfo('description'); ?></p>
+
+<?php
+	$names = array('brad', 'jon', 'lisa');
+?>
+
+<p>hi my name is <?php echo $names[0]; ?></p>
+
+<?php 
+	$count = 0;
+	while ($count < count($names) ) {
+		echo "<li> hi, my name is $names[$count] </li>";
+		$count++;
+	}
+ ?>
