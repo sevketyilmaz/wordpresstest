b3b9c4377dc0cd39aec0dae95143b61bcb61fe4c
sevketyilmaz <sevketyilmaz@gmail.com>
2018-05-21 17:26:10 +0000

    04-011-Adding SytleSheet.

--- wp-content/themes/university-theme/header.php
+++ wp-content/themes/university-theme/header.php
@@ -1,4 +1,11 @@
-<p><b>"header.php"</b></p>
-<p>
-<h1><?php bloginfo('name'); ?></h1>
-<p><?php bloginfo('description'); ?></p>
+<!DOCTYPE html>
+<html>
+<head>
+	<?php wp_head(); ?>
+</head>
+<body>
+	<h1><?php bloginfo('name'); ?></h1>
+	<p><?php bloginfo('description'); ?></p>
+	<p><b>"header.php"</b></p>
+</body>
+</html>
